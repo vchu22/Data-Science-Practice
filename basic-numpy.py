01d833e2edcfa51912738750453ee2c92914d272
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2018-06-23 18:22:10 -0400

    added numpy practice

---
+++ basic-numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+numbers = raw_input("Type in a list of numbers seperate by space: ")
+numbers = map(int, numbers.split())
+print numbers
+print "The average of your numbers is", np.mean(numbers)
+print "The median of your numbers is", np.median(numbers)
+print "The standard deviation of your numbers is", np.std(numbers)
