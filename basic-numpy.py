6cc3942dfd8bf240af8839f4b682feb6c7efce7f
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2018-08-14 22:09:39 -0400

    added map, lambda, and np.array practice

--- basic-numpy.py
+++ basic-numpy.py
@@ -2,6 +2,13 @@ import numpy as np
 numbers = raw_input("Type in a list of numbers separated by space: ")
 numbers = map(int, numbers.split()) # seperate the string of numbers using space character delimiter
 print numbers
-print "The average of your numbers is", np.mean(numbers)
-print "The median of your numbers is", np.median(numbers)
-print "The standard deviation of your numbers is", np.std(numbers)
+if len(numbers) > 0:
+    print "The average of your numbers is", np.mean(numbers)
+    print "The median of your numbers is", np.median(numbers)
+    print "The standard deviation of your numbers is", np.std(numbers)
+
+numbers2 = map(lambda x: x*2, numbers)  # lambda = anonymous function
+numbers3 = map(lambda x: x**2, numbers)
+print "The numbers times 2 is ",numbers2
+print "The numbers to the power of 2 is ",numbers3
+print "The 2D array of all these is:\n",np.array([numbers, numbers2, numbers3], float)
