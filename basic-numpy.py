8276e20d04f2f9593518975c0c61ecd7f9fe9d07
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2018-08-14 21:35:32 -0400

    added comment

--- basic-numpy.py
+++ basic-numpy.py
@@ -1,6 +1,6 @@
 import numpy as np
 numbers = raw_input("Type in a list of numbers separated by space: ")
-numbers = map(int, numbers.split())
+numbers = map(int, numbers.split()) # seperate the string of numbers using space character delimiter
 print numbers
 print "The average of your numbers is", np.mean(numbers)
 print "The median of your numbers is", np.median(numbers)
