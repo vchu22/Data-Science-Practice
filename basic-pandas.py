0645435919f5391d80e3ce7628dac0440261022b
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2018-08-16 19:43:43 -0400

    added filters

--- basic-pandas.py
+++ basic-pandas.py
@@ -4,5 +4,11 @@ data = {'name':pd.Series(['John','Alice','Susan','Tim','James','Dave']),
         'hoursStudied':pd.Series([10,8,10,2,7,6]),
         'grade':pd.Series([88,90,70,58,74,95])
        }
-dataframe = pd.DataFrame(data, columns=['name','age','hoursStudied','grade'])
-print dataframe
+gradebook = pd.DataFrame(data, columns=['name','age','hoursStudied','grade'])
+print "Grades for the first exam\n", gradebook
+print
+passed = gradebook[gradebook.grade>=60][['name','grade']]
+print "People who passed the test\n",passed
+print
+yfailed = gradebook[(gradebook.grade<60) & (gradebook.age<30)][['name','grade']]
+print "Young people who failed the test\n",yfailed
